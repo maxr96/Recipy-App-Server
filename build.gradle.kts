a112fd6d41a890c7dc51e4d322cf45eb665125b0
Maksym Radchuk <maxr96@gmail.com>
2020-09-21 19:07:48 +0200

    Kotlin update to 1.4. Gradle update to 6.7RC-1

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,13 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.3.4.RELEASE"
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
-    kotlin("jvm") version "1.3.72"
-    kotlin("plugin.spring") version "1.3.72"
+    kotlin("jvm") version "1.4.10"
+    kotlin("plugin.spring") version "1.4.10"
 }
 
 group = "com.recipyApp"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_13
 
 repositories {
     mavenCentral()
@@ -46,6 +46,6 @@ tasks.withType<Test> {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "11"
+        jvmTarget = "13"
     }
 }
--- src/main/kotlin/com/recipyapp/recipyserver/RecipyserverApplication.kt
+++ src/main/kotlin/com/recipyapp/recipyserver/RecipyserverApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class RecipyServerApplication
+class RecipyserverApplication
 
 fun main(args: Array<String>) {
-    runApplication<RecipyServerApplication>(*args)
+    runApplication<RecipyserverApplication>(*args)
 }
