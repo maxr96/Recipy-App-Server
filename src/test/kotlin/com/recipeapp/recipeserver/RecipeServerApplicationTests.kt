573efd9ac358bf6e4abcda8d67a80193e52d1f84
Maksym Radchuk <maxr96@gmail.com>
2020-12-23 16:06:40 +0100

    :rotating_light: Fix build fail on Github CI.

--- src/test/kotlin/com/recipeapp/recipeserver/RecipeServerApplicationTests.kt
+++ src/test/kotlin/com/recipeapp/recipeserver/RecipeServerApplicationTests.kt
@@ -2,8 +2,10 @@ package com.recipeapp.recipeserver
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RecipeServerApplicationTests {
 
     @Test
